175f84d4bf55f4f1913360af251a55b4e4c22a16
Chris Soncrant <christopher.soncrant@gmail.com>
2021-12-03 09:56:28 -0800

    created the show/hide button and the login button

--- script.js
+++ script.js
@@ -2,57 +2,69 @@ let labels = document.querySelectorAll(".input-container label");
 
 let inputs = document.querySelectorAll(".input-container input");
 
-let logInBtn = document.querySelector('input[type="submit"]')
+let logInBtn = document.querySelector('input[type="submit"]');
 
 
 
-
+//Setting the Show Hide Button
 const showHideContainer = document.getElementById('show-hide-container');
 
 let showHideBtn = document.createElement('button');
 
-showHideBtn.classList.add("check");
+showHideBtn.setAttribute('type', 'button');
 
 showHideBtn.textContent = "Show";
 
-showHideContainer.appendChild(showHideBtn);
-
-
-
-console.log(labels);
-
-console.log(showHideBtn);
-
-let check = 0;
-
-logInBtn.addEventListener('click', () => {
-    if (!check) {
-        showHideContainer.removeChild(showHideBtn);
-        check++;
+showHideBtn.addEventListener('click', () => {
+    if (showHideBtn.textContent === "Show") {
+        showHideBtn.textContent = "Hide";
+        document.getElementById("password").type = "text";
     } else {
-        showHideContainer.appendChild(showHideBtn);
-        check--;
-    }  
+        showHideBtn.textContent = "Show";
+        document.getElementById("password").type = "password";
+    }
 })
 
+// console.log(labels);
 
-inputs.forEach(input => {
+// console.log(showHideBtn);
+
+//Login Button check variables
+let check1 = 0;
+let check2 = 0;
+
+inputs.forEach(input => {   
+    console.log('test');
     input.addEventListener('input', () => {
         if (input.id === "username") {
             if (input.value === "") {
                 labels[0].classList.remove('triggered');
+                check1 = 0;
             } else {
                 labels[0].classList.add('triggered');
-            }
-        }
+                check1 = 1;
+            };
+        };
 
         if (input.id === "password") {
             if (input.value === "") {
                 labels[1].classList.remove('triggered');
+                showHideContainer.removeChild(showHideBtn);
+                check2 = 0;
             } else {
                 labels[1].classList.add('triggered');
-            }
-        }
+                showHideContainer.appendChild(showHideBtn);
+                check2 = 1;
+            };
+        };
 
-    })
+        if (check1 && check2) {
+            logInBtn.style.backgroundColor = "blue";
+            logInBtn.style.cursor = "pointer";
+        } else {
+            logInBtn.style.backgroundColor = "#0095f64d";
+            logInBtn.style.cursor = "default";
+        }
+    });
 });
+
